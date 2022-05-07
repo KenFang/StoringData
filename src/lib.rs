1318de53c98f1357b49f935e9141821af0d354b3
KenFang <kenfangch@yahoo.com.tw>
2022-05-07 16:00:30 +0800

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,19 @@ use actix_web::dev::Server;
 use std::net::TcpListener;
 
 async fn health_check() -> HttpResponse {
+
+    HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+// Let's start simple: We always return a 200 Ok
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+
     HttpResponse::Ok().finish()
 }
 
@@ -11,6 +24,8 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(||  {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            // A new entry in our routing table for POST /subscriptions requests
+            .route("/subscriptions", web::post().to(subscribe))
     })
         .listen(listener)?
         .run();
